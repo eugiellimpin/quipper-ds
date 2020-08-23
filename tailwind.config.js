016c6f477a8ed68f36c6b3941bbb0fe8c907aff8
Eugie Limpin <eugiellimpin@gmail.com>
2020-08-23 09:01:52 +0800

    Override theme instead of extending

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,21 @@
-var theme = require('./theme.js');
+var theme = require("./theme.js");
 
 module.exports = {
   purge: [],
   theme: {
-    extend: {
-      fontFamily: theme.fonts.body,
-      colors: theme.colors,
-      borderRadius: theme.borderRadius,
-      boxShadow: theme.shadows
+    fontFamily: theme.fonts.body,
+    colors: theme.colors,
+    borderRadius: theme.borderRadius,
+    boxShadow: theme.shadows,
+    spacing: {
+      0: "0px",
+      4: "4px",
+      8: "8px",
+      14: "14px",
+      16: "16px",
+      48: "48px",
     },
   },
   variants: {},
   plugins: [],
-}
+};
