3516644b60ddc9bc516371521f6f1d61d0eff46c
Miguel N. Galace <galacemiguel@gmail.com>
2020-09-21 10:39:05 +0800

    Add space key to theme object

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,16 +8,7 @@ module.exports = {
     colors: theme.colors,
     borderRadius: theme.borderRadius,
     boxShadow: theme.shadows,
-    spacing: {
-      0: "0px",
-      4: "4px",
-      8: "8px",
-      14: "14px",
-      16: "16px",
-      40: "40px",
-      48: "48px",
-      64: "64px",
-    },
+    spacing: theme.space,
     transitionProperty: {
       'shadow': 'box-shadow',
       'bg': 'background-color',
