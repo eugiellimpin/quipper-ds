20baae2eeb844253b68a736794fc5dd815bfc4a3
Miguel N. Galace <galacemiguel@gmail.com>
2020-11-16 14:24:38 +0800

    Add color to base styles plugin

--- src/plugins.js
+++ src/plugins.js
@@ -3,6 +3,7 @@ const plugin = require("tailwindcss/plugin");
 const baseStyles = plugin(function ({ addBase, config }) {
   addBase({
     ":root": {
+      color: config("theme.colors.gray60"),
       fontSize: config("theme.fontSize.14"),
       fontFamily: config("theme.fontFamily.body").join(", "),
     },
